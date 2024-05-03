9acf4d29497a15edba2dda644744b40a610be826
saraUsama9910 <sarausama80@gmail.com>
2024-05-03 18:27:03 +0300

    remove test error

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 class FoodApp extends StatefulWidget {
-  FoodApp._internal();//named constructor
-  static final FoodApp _instance = FoodApp._internal();//singleton or single instance
-factory FoodApp()=>_instance;//factory
+  FoodApp._internal(); //named constructor
+  int appState = 0;
+  static final FoodApp _instance =
+      FoodApp._internal(); //singleton or single instance
+  factory FoodApp() => _instance; //factory
   @override
   State<FoodApp> createState() => _FoodAppState();
 }
@@ -11,6 +13,7 @@ factory FoodApp()=>_instance;//factory
 class _FoodAppState extends State<FoodApp> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container();
   }
 }
+
